261fd65649dfac1f602758777b417e7402ec7fbe
Megan Nelson <menelson@vassar.edu>
2019-05-29 19:56:29 +0000

    styled with semantic ui and refactored

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require("express"),
-bodyParser = require("body-parser"),
-mongoose = require("mongoose"),
-methodOverride = require("method-override"),
-app = express();
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override"),
+    //expressSanitizer = require("express-sanitizer"),
+    app = express();
 
 //app config
 mongoose.connect("mongodb://localhost/blogApp");
 app.set("view engine", "ejs");
 app.use(express.static("public")); //for serving custom style sheets
 app.use(bodyParser.urlencoded({extended: true}));
+//app.use(expressSanitizer);
 app.use(methodOverride("_method"));
 
 // mongoose model config
@@ -23,17 +25,42 @@ var blog_scheme = new mongoose.Schema({
 var Blog = mongoose.model("Blog", blog_scheme);
 
 
-// Blog.create({
-//   title: "This is a blog",
-//   image: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-//   body: "Hey there, hey hey hey hey, hery eheye hey"
-// }, function(err, ne) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log("HEY" + ne);
-//     }
-// });
+Blog.create({
+  title: "Wowwww",
+  image: "https://images.unsplash.com/photo-1534278931827-8a259344abe7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+  body: "Wow? Can you believe it. Neither can I. When you stepped out onto the scene... I thought..."
+}, function(err, ne) {
+    if (err) {
+         console.log(err);
+    } else {
+         console.log("HEY" + ne);
+    }
+});
+
+Blog.create({
+  title: "Hello!!!",
+  image: "https://sbly-web-prod-shareably.netdna-ssl.com/wp-content/uploads/2017/10/18150512/Screen-Shot-2017-10-18-at-3.03.52-PM.png",
+  body: "Wow? Can you believe it. Neither can I. When you stepped out onto the scene... I thought..."
+}, function(err, ne) {
+    if (err) {
+         console.log(err);
+    } else {
+         console.log("HEY" + ne);
+    }
+});
+
+Blog.create({
+  title: "Hello!!!",
+  image: "https://www.denverpost.com/wp-content/uploads/2018/01/day_in_pictures_010818_001.jpg?w=620",
+  body: "Wow? Can you believe it. Neither can I. When you stepped out onto the scene... I thought..."
+}, function(err, ne) {
+    if (err) {
+         console.log(err);
+    } else {
+         console.log("HEY" + ne);
+    }
+});
+
 
 
 //RESTful routes
@@ -62,6 +89,7 @@ app.get("/blogs/new", function(req, res){
 
 //CREATE route
 app.post("/blogs", function(req, res){
+  //  req.body.blog.body = req.sanitize(req.body.blog.body);
    Blog.create(req.body.blog, function(err, newBlog){
        if (err) {
            res.render("new");
@@ -100,6 +128,7 @@ app.get("/blogs/:id", function(req, res) {
 //update
 
 app.put("/blogs/:id", function(req, res) {
+   //req.body.blog.body = req.sanitize(req.body.blog.body);
    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
       if (err) {
           res.redirect("/blogs");
@@ -107,6 +136,18 @@ app.put("/blogs/:id", function(req, res) {
           res.redirect("/blogs/" + req.params.id);
       }
    });
+});
+
+//destroy route
+
+app.delete("/blogs/:id", function(req, res) {
+   Blog.findByIdAndRemove(req.params.id, function(err) {
+       if (err) {
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
+       }
+   })
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
